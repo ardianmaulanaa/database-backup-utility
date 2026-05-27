23868305d47642790668c8c239ca62ce543a4bca
Muhammad Ardian Maulana <ardianmaulana92251@gmail.com>
2026-05-27 16:22:21 +0700

    New Update mongodb restore, redis backup

--- main.go
+++ main.go
@@ -96,16 +96,94 @@ func restoreMongoDB() {
 
 	checkMongoDumpVersion()
 
-	fmt.Println("MongoDB restore process will be implemented here.")
-	logger.WriteLog("MongoDB restore process executed")
+	if len(os.Args) < 4 {
+		fmt.Println("Please provide backup folder path.")
+		fmt.Println("Example:")
+		fmt.Println("  go run main.go mongodb restore backups/mongodb_2026-05-27_14-30-10")
+		logger.WriteLog("MongoDB restore failed: backup path not provided")
+		return
+	}
+
+	backupPath := os.Args[3]
+
+	if !checkMongoRestoreVersion() {
+		fmt.Println("MongoDB restore stopped because mongorestore is not available.")
+		logger.WriteLog("MongoDB restore stopped because mongorestore is not available")
+		return
+	}
+
+	_, err := os.Stat(backupPath)
+	if os.IsNotExist(err) {
+		fmt.Println("Backup folder not found:", backupPath)
+		logger.WriteLog("MongoDB restore failed: backup folder not found: " + backupPath)
+		return
+	}
+
+	cmd := exec.Command("mongorestore", backupPath)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("MongoDB restore failed:", err)
+		fmt.Println(string(output))
+		logger.WriteLog("MongoDB restore failed: " + err.Error())
+		return
+	}
+
+	fmt.Println("MongoDB restore completed successfully")
+	fmt.Println(string(output))
+	logger.WriteLog("MongoDB restore completed successfully from: " + backupPath)
 }
 
 func backupRedis() {
 	fmt.Println("Starting Redis backup...")
 	logger.WriteLog("Starting Redis backup")
 
-	fmt.Println("Redis backup process will be implemented here.")
-	logger.WriteLog("Redis backup process executed")
+	if !checkRedisCLI() {
+		fmt.Println("Redis backup stopped because redis-cli not available")
+		logger.WriteLog("Redis backup stopped because redis-cli is not available")
+		return
+	}
+
+	timestamp := time.Now().Format("2006-01-02_15-04-05")
+	backupPath := "backup/redis_" + timestamp
+
+	err := os.MkdirAll(backupPath, 0755)
+	if err != nil {
+		fmt.Println("Failed to create Redis backup folder", err)
+		logger.WriteLog("Redis backup failed: " + err.Error())
+		return
+	}
+
+	cmdSave := exec.Command("redis-cli", "SAVE")
+	output, err := cmdSave.CombinedOutput()
+	if err != nil {
+		fmt.Println("Failed to save Redis data:", err)
+		fmt.Println(string(output))
+		logger.WriteLog("Redis SAVE failed: " + err.Error())
+		return
+	}
+
+	sourceFile := "/opt/homebrew/var/db/redis/dump.rdb"
+	destinationFile := backupPath + "/dump.rdb"
+
+	input, err := os.ReadFile(sourceFile)
+	if err != nil {
+		fmt.Println("Failed to read Redis dump file:", err)
+		logger.WriteLog("Redis backup failed while reading dump.rdb: " + err.Error())
+		return
+	}
+
+	err = os.WriteFile(destinationFile, input, 0644)
+
+	if err != nil {
+		fmt.Println("Failed to write Redis backup file:", err)
+		logger.WriteLog("Redis backup failed while writing dump.rdb: " + err.Error())
+		return
+	}
+
+	fmt.Println("Redis backup completed successfully")
+	fmt.Println("Backup saved to:", destinationFile)
+	logger.WriteLog("Redis backup completed successfully: " + destinationFile)
 }
 
 func restoreRedis() {
@@ -129,6 +207,38 @@ func checkMongoDumpVersion() {
 
 	fmt.Println(string(output))
 	logger.WriteLog("mongodump is available")
+}
+
+func checkRedisCLI() bool {
+	fmt.Println("Checking redis-cli version")
+
+	cmd := exec.Command("redis-cli", "--version")
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("redis-cli is not available:", err)
+		logger.WriteLog("redis-cli check failed: " + err.Error())
+		return false
+	}
+	fmt.Println(string(output))
+	logger.WriteLog("redis-cli is available")
+	return true
+}
+
+func checkMongoRestoreVersion() bool {
+	fmt.Println("Checking mongorestore version")
+
+	cmd := exec.Command("mongorestore", "--version")
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("mongorestore is not available:", err)
+		logger.WriteLog("mongorestore check failed: " + err.Error())
+		return false
+	}
+
+	fmt.Println(string(output))
+	logger.WriteLog("mongorestore is available")
+	return true
 }
 
 func showHelp() {
